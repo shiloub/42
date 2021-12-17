a79225040e22d195fca0db3d6edd2c411239773b
Achille MONTANTIN <amontant@e2r3p19.clusters.42paris.fr>
2021-12-17 19:36:53 +0100

    PUSH PUSH

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -6,14 +6,9 @@
 /*   By: amontant <amontant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 12:22:22 by amontant          #+#    #+#             */
-/*   Updated: 2021/12/14 19:14:43 by amontant         ###   ########.fr       */
+/*   Updated: 2021/12/16 16:11:04 by amontant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// #include <stdlib.h>
-// #include <stdio.h>
-// #include <ctype.h>
-// #include <unistd.h>
 
 int	ft_isalnum(int c)
 {
@@ -24,18 +19,3 @@ int	ft_isalnum(int c)
 	else
 		return (0);
 }
-// int main()
-// {
-// 	int i = -1;
-// 	// while( i < 530)
-// 	// {
-// 	// 	printf("%d ok: %d\n",i,ft_isalnum(i));
-// 	// 	printf("%d !ok: %d\n",i,isalnum(i));
-// 	// 	i++;
-// 	// }
-// 	write(1,"HAH\n",4);
-// 	i = 262;
-// 	write(1,&i,1);
-// 	write(1,"HAH\n",4);
-
-// }
