8c3ec78503a1dcb3b21f1834c9558cf2ad3eec8f
Achille MONTANTIN <amontant@e1r5p12.42.fr>
2021-12-21 15:23:59 +0100

    pret a push

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: amontant <amontant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 17:34:47 by amontant          #+#    #+#             */
-/*   Updated: 2021/12/18 16:30:26 by amontant         ###   ########.fr       */
+/*   Updated: 2021/12/21 15:19:46 by amontant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdio.h>
 #include <stddef.h>
 
 char	*save_until_cr(char *str)
@@ -22,14 +21,12 @@ char	*save_until_cr(char *str)
 	int	i;
 
 	i = 0;
-	//printf("** str avant modif = %s", str);
 	while (str[i + 1] && str[i] != '\n')
 	{
 		i++;
 	}
 	if (str[i] == '\n')
 		str[i + 1] = 0;
-	//printf("str  apres modif = %s\n", str);
 	return (str);
 }
 
@@ -59,10 +56,10 @@ char	*get_next_line(int fd)
 	char		*newline;
 	int			fin;
 
-	newline = ft_strdup("");
-	newline = ft_strjoin_f(newline, buff);
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, "", 0) == -1)
 		return (NULL);
+	newline = ft_strdup("");
+	newline = ft_strjoin_f(newline, buff);
 	while (ft_strchr(buff, '\n') == 0)
 	{
 		fin = read(fd, buff, BUFFER_SIZE);
@@ -80,31 +77,3 @@ char	*get_next_line(int fd)
 	save_after_cr(buff);
 	return (newline);
 }
-/*int main()
-{
-	int i = 0;
-	char *str;
-	int fd;
-	fd = open("text.txt", O_RDONLY);
-	str = get_next_line(fd);
-	printf("%s", str);
-	str = get_next_line(fd);
-	printf("%s", str);
-	str = get_next_line(fd);
-	printf("%s", str);
-	str = get_next_line(fd);
-	printf("%s", str);
-	str = get_next_line(fd);
-	printf("%s", str);
-	str = get_next_line(fd);
-	printf("%s", str);
-	str = get_next_line(fd);
-	printf("%s", str);
-	str = get_next_line(fd);
-	printf("%s", str);
-	str = get_next_line(fd);
-	printf("%s", str);
-	
-	return 0;
-
-}*/
