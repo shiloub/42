c8b073e6355d3ab94a85113c21c4ca6b85650483
shiloub <achillemo@hotmail.fr>
2021-12-23 13:51:04 +0100

    makefile mis a jour

--- Printf/main.c
+++ Printf/main.c
@@ -4,8 +4,8 @@
 int main()
 {
 	int i;
-	i = ft_printf("salut je m'appelle %s et j'ai %d%s et le pointeur de i = %p", "jon", 22, "ans", &i);
-	ft_printf("\n retour = %d\n", i);
-	i = printf("salut je m'appelle %s et j'ai %d%s et le pointeur de i = %p", "jon", 22, "ans", &i);
+	// i = ft_printf("salut je m'appelle % et j'ai %d%s et le pointeur de i = %p", "jon", 22, "ans", &i);
+	// ft_printf("\n retour = %d\n", i);
+	i = printf("salut je m'appelle % et j'ai %d%s et le pointeur de i = %p", "jon", 22, "ans", &i);
 	printf("\n retour = %d\n", i);
 }
