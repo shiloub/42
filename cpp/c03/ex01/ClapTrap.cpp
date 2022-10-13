002406f5d242374f8aefd1079492bd0227987d80
shiloub <achillemo@hotmail.fr>
2022-10-13 13:23:22 +0200

    recover

--- cpp/c03/ex01/ClapTrap.cpp
+++ cpp/c03/ex01/ClapTrap.cpp
@@ -2,7 +2,7 @@
 
 ClapTrap::ClapTrap() : _hit_points(10), _energy_point(10), _attack_damage(0)
 {
-	_hit_points = 10;
+	_name = "random";
 	std::cout << "Default constructor called\n";
 }
 
@@ -75,3 +75,42 @@ void	ClapTrap::beRepaired(unsigned int amount)
 	this->_hit_points += amount;
 }
 
+unsigned int	ClapTrap::get_hit_point()
+{
+	return (this->_hit_points);
+}
+
+unsigned int	ClapTrap::get_attack_damage()
+{
+	return (this->_attack_damage);
+}
+
+unsigned int	ClapTrap::get_energy_point()
+{
+	return (this->_energy_point);
+}
+
+std::string	ClapTrap::get_name()
+{
+	return (this->_name);
+}
+
+void	ClapTrap::set_name(std::string name)
+{
+	this->_name = name;
+}
+
+void	ClapTrap::set_energy_point(unsigned int energy_point)
+{
+	this->_energy_point = energy_point;
+}
+
+void	ClapTrap::set_attack_damage(unsigned int attack_damage)
+{
+	this->_attack_damage = attack_damage;
+}
+
+void	ClapTrap::set_hit_point(unsigned int hit_point)
+{
+	this->_hit_points = hit_point;
+}
