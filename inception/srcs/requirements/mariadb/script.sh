5b623ede5f6056190a028e8743b0e31842fc73e0
shiloub <achillemo@hotmail.fr>
2023-03-03 18:02:27 +0100

    message

--- inception/srcs/requirements/mariadb/script.sh
+++ inception/srcs/requirements/mariadb/script.sh
@@ -5,24 +5,24 @@
 # sleep 20;
 # exit;
 service mysql start
-# mysql_secure_installation << EOF
-# ${ADMIN_PASS}
-# n
-# y
-# n
-# y
-# y
-# EOF
+mysql_secure_installation << EOF
+${ADMIN_PASS}
+n
+y
+n
+y
+y
+EOF
 
 mysql -u root -p${ADMIN_PASS} << EOF
 CREATE DATABASE IF NOT EXISTS db_db ;
-SHOW DATABASES ;
-DROP USER shiloub@wordpress.srcs_inception;
 FLUSH PRIVILEGES;
-CREATE USER "shiloub"@"wordpress.srcs_inception" IDENTIFIED BY "$MYSQL_DB_PASS";
+CREATE USER IF NOT EXISTS "shiloub"@"wordpress.srcs_inception" IDENTIFIED BY "$MYSQL_DB_PASS";
 GRANT ALL PRIVILEGES ON db_db.* TO "shiloub"@"wordpress.srcs_inception" IDENTIFIED BY "$MYSQL_DB_PASS";
 FLUSH PRIVILEGES;
 EOF
 sleep 3
 service mysql stop
 exec mysqld
+
+# DROP USER shiloub@wordpress.srcs_inception;
